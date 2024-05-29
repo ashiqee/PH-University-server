a8f57be65d9b9dec49b351eecbcff872333a31ea
ashiq73 <ashiq.buet73@gmail.com>
2024-05-29 11:02:56 +0600

    academic model and schema done

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,32 +1,40 @@
 import { Schema, model } from "mongoose";
-import { TAcademicSemester, TMonths } from "./academicSemester.interface";
+import { TAcademicSemester, TAcademicSemesterCode, TAcademicSemesterName, TMonths } from "./academicSemester.interface";
 
-const months: TMonths[] = [
+const Months: TMonths[] = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ]
+
+  const AcademicSemesterName :TAcademicSemesterName[] = ["Autumn", "Summer", "Fall"];
+  const AcademicSemesterCode :TAcademicSemesterCode[] = ["01", "02", "03"];
 
 const academicSemesterSchema= new Schema<TAcademicSemester>(
     {
         name:{
             type: String,
             required:true,
+            enum: AcademicSemesterName,
         },
         year:{
             type: Date,
             required:true,
+            
         },
         code:{
             type: String,
             required:true,
+            enum:AcademicSemesterCode,
         },
         startMonth:{
             type: String,
-            enum: months,
+            required:true,
+            enum: Months,
         },
         endMonth:{
             type: String,
-            enum: months,
+            required:true,
+            enum: Months,
         },
     },
     {
