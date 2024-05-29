a217e75fd5894d9e2516a1292efab8fb732e675c
ashiq73 <ashiq.buet73@gmail.com>
2024-05-29 11:13:16 +0600

    [validation]-z validation for academic semester

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,13 +1,8 @@
 import { Schema, model } from "mongoose";
-import { TAcademicSemester, TAcademicSemesterCode, TAcademicSemesterName, TMonths } from "./academicSemester.interface";
+import { AcademicSemesterCode, AcademicSemesterName, Months } from "./academicSemester.constant";
+import { TAcademicSemester } from "./academicSemester.interface";
 
-const Months: TMonths[] = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ]
 
-  const AcademicSemesterName :TAcademicSemesterName[] = ["Autumn", "Summer", "Fall"];
-  const AcademicSemesterCode :TAcademicSemesterCode[] = ["01", "02", "03"];
 
 const academicSemesterSchema= new Schema<TAcademicSemester>(
     {
