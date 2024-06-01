57fff7c4bbc972a75745b67026f606e3009d9041
ashiqee <ashiq.buet73@gmail.com>
2024-06-01 06:56:17 +0600

    academic faculty service done

---
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,42 @@
+import { TAcademicFaculty } from "./academicFaculty.interface";
+import { AcademicFaculty } from "./academicFaculty.model";
+
+
+const createAcademicFacultyInDB = async (payload: TAcademicFaculty)=>{
+    const result = AcademicFaculty.create(payload)
+    return result;
+};
+
+const getAllAcademicFacultyFromDB = async ()=>{
+
+const result = AcademicFaculty.find()
+return result;
+}
+
+
+const getSingleAcademicFacultyFromDB = async(id: string)=>{
+    
+    const  result = AcademicFaculty.findById(id)
+    return result;
+
+}
+
+
+const updateAcademicFacultyInDB = async(id: string,updateData: Partial<TAcademicFaculty> )=>{
+    
+
+    const result = AcademicFaculty.findOneAndUpdate(
+        {_id:id},
+        {$set:updateData},
+        {new: true})
+
+        return result;
+}
+
+
+export const  AcademicSevices = {
+    createAcademicFacultyInDB,
+    getAllAcademicFacultyFromDB,
+    getSingleAcademicFacultyFromDB,
+    updateAcademicFacultyInDB
+}
