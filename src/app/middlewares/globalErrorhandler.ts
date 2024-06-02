563d3a3dc514c928f65c728ada68da08d0e4b061
ashiq73 <ashiq.buet73@gmail.com>
2024-06-02 16:18:55 +0600

    add error instanceof

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,17 +1,37 @@
-
-import { Request, Response } from 'express';
+import { ErrorRequestHandler, Request, Response } from 'express';
 import { NextFunction } from 'express';
-const globalErrorHandler = (err: any,req:Request,res:Response,next:NextFunction)=>{
-    const statusCode = err.statusCode || 500
-    const message =err.message || "Something went wrong"
-    return res.status(statusCode).json({
-      success: false,
-      status: statusCode,
-      message,
-      error: err
-    })
+import { ZodError } from 'zod';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  //setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+  type TErrorsources = {
+    path?: string | number;
+    message: string;
+  }[];
+  let errorSources: TErrorsources = [
+    { path: '', message: 'Something went wrong' },
+  ];
+
+const handleZodError = (err: ZodError) => {
+  const formattedErrors: TErrorsources = err.issues.map((issue) => ({
+    path: issue.path[issue.path.length - 1],
+    message: issue.message,
+  }));
+  return formattedErrors;  
+};
+
+  if(err instanceof ZodError){
+    statusCode= 400
+    message= err.issues[0].message
   }
 
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    amiError: err
+  });
+};
 
-
-  export default globalErrorHandler;
+export default globalErrorHandler;
