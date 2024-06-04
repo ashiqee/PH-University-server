e440078cc98d66c7034ce72741c54a21ccd3a201
ashiqee <ashiq.buet73@gmail.com>
2024-06-04 06:52:57 +0600

    unhandleRejection and uncaghtExpeption error handle

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -7,10 +7,11 @@ import handleZodError from './../errors/handleZodError';
 import handleValidationError from './../errors/handleValidationError';
 import handleCastError from './../errors/handleCastError';
 import handleDuplicateError from './../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   //setting default values
-  let statusCode = err.statusCode || 500;
-  let message = err.message || 'Something went wrong';
+  let statusCode =  500;
+  let message = 'Something went wrong';
 
 
 
@@ -43,6 +44,22 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiederror?.statusCode;
     message = simplifiederror?.message;
     errorSources = simplifiederror?.errorSources;
+  
+  }else if (err instanceof AppError) {
+
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [{
+      path:'',
+      message:"err?.message",
+    }]
+  }else if (err instanceof Error) {
+
+    message = err?.message;
+    errorSources = [{
+      path:'',
+      message:"err?.message",
+    }]
   }
 
   return res.status(statusCode).json({
