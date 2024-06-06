8af6b973f9dd2423466c8ec99c7a6195f271024c
ashiqee <ashiq.buet73@gmail.com>
2024-06-06 20:17:12 +0600

    facylties model and interface done

--- src/app/modules/faculties/faculties.interface.ts
+++ src/app/modules/faculties/faculties.interface.ts
@@ -4,7 +4,19 @@ import { Types } from 'mongoose';
 export type TFaculties={
     id: string,
     name: string,
+    designation: string,
+    gender:string,
+    dateOfBirth: string,
+    email: string,
+    contactNo: string,
+    emergencyContactNo: string,
+    presentAddress:string,
+    permanentAddress:string,
+    profileImage: string,
     academicFaculty:  Types.ObjectId,
+    academicDepartment: Types.ObjectId,
+    isDeleted: boolean,
+
 }
 
 
--- src/app/modules/faculties/faculties.model.ts
+++ src/app/modules/faculties/faculties.model.ts
@@ -5,11 +5,62 @@ import { TFaculties } from "./faculties.interface";
 
 const facultiesSchema =  new Schema<TFaculties>(
     {
+        id: { type: String,
+        required: [true, 'ID is required'],
+        unique: true },
+        designation: {
+            type: String,
+            require: [true, 'Designation is required'],
+        },
         name:{
             type: String,
-            required:true,
-            unique: true
-        }
+            require: [true, 'Name is required'],
+           
+        },
+        gender: {
+            gender: ['male', 'female'],
+            require: [true,'gender is required']
+        },
+        dateOfBirth: {
+            type: String,
+            require: [true,'dateOfBirth is required']
+        },
+        email: {
+            type: String,
+            require: [true,'email is required']
+        },
+        contactNo: {
+            type: String,
+            require: [true,'contactNo is required']
+        },
+              emergencyContactNo: {
+            type: String,
+            require: [true,'emergencyContactNo is required']
+        },
+        presentAddress: {
+            type: String,
+            require: [true,'presentAddress is required']
+        },
+        permanentAddress: {
+            type: String,
+            require: [true,'permanentAddress is required']
+        },
+        profileImage: {
+            type: String,
+            require: [true,'profileImage is required']
+        },
+        academicFaculty: {
+            type: Schema.Types.ObjectId,
+            require: [true,'academicFaculty is required']
+        },
+        academicDepartment: {
+            type: Schema.Types.ObjectId,
+            require: [true,'academicDepartment is required']
+        },
+        isDeleted: {
+            type: Boolean,
+            default:false,
+        },
     },{
         timestamps:true
     }
