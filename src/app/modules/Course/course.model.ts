057c6096a7189219d6c48ddda4b22c661de3184a
ashiqee <ashiq.buet73@gmail.com>
2024-06-07 14:49:14 +0600

    course model and interface create

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+import { TCourse, TPrerequisiteCourses } from "./course.interface";
+
+
+const preRequisiteCourseSchema = new Schema<TPrerequisiteCourses>({
+    course: {
+        type: Schema.Types.ObjectId,
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false,
+    }
+})
+
+
+const courseSchema = new Schema<TCourse>({
+    title:{
+        type: String,
+        unique:true,
+        trim:true,
+        required:true,
+    },
+    prefix:{
+        type: String,
+        
+        trim:true,
+        required:true,
+    },
+    code:{
+        type: Number,
+        
+        trim:true,
+        required:true,
+    },
+    credits:{
+        type: Number,
+        
+        trim:true,
+        required:true,
+    },
+    preRequisiteCourses:[preRequisiteCourseSchema],
+})
+
+export const Course = model<TCourse>('Course', courseSchema)
